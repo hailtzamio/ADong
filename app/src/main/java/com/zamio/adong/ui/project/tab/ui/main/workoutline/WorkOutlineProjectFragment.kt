f50c41566400f63d9ed9e6fbc62ac1eb617eba4a
hailt <you@example.comhailtdev@gmail.com>
2020-05-27 16:47:09 +0700

    cm

--- app/src/main/java/com/zamio/adong/ui/project/tab/ui/main/workoutline/WorkOutlineProjectFragment.kt
+++ app/src/main/java/com/zamio/adong/ui/project/tab/ui/main/workoutline/WorkOutlineProjectFragment.kt
@@ -8,6 +8,7 @@ import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -21,6 +22,8 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.elcom.com.quizupapp.ui.fragment.BaseFragment
 import com.elcom.com.quizupapp.ui.network.RestData
+import com.esafirm.imagepicker.features.ImagePicker
+import com.esafirm.imagepicker.model.Image
 import com.google.gson.JsonElement
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
@@ -76,32 +79,15 @@ class MainWorkOutlineFragment : BaseFragment() {
         return inflater.inflate(R.layout.fragment_main_workeoutline, container, false)
     }
 
-
-    var PICK_IMAGE_MULTIPLE = 3000
-    lateinit var imagePath: String
-    var imagesPathList: MutableList<String> = arrayListOf()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         getData(0)
 
-
         tvOk.setOnClickListener {
-            if (Build.VERSION.SDK_INT < 19) {
-                var intent = Intent()
-                intent.type = "image/*"
-                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-                intent.action = Intent.ACTION_GET_CONTENT
-                startActivityForResult(
-                    Intent.createChooser(intent, "Select Picture")
-                    , PICK_IMAGE_MULTIPLE
-                )
-            } else {
-                var intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-                intent.addCategory(Intent.CATEGORY_OPENABLE)
-                intent.type = "image/*"
-                startActivityForResult(intent, PICK_IMAGE_MULTIPLE);
-            }
+            ImagePicker.create(this).multi()
+                .toolbarImageTitle("Chọn 8 ảnh")
+                .limit(3)
+                .start()
         }
     }
 
@@ -262,21 +248,11 @@ class MainWorkOutlineFragment : BaseFragment() {
             }
         }
 
-        if (requestCode == PICK_IMAGE_MULTIPLE && resultCode == Activity.RESULT_OK
-            && null != data1
-        ) {
-            if (data1.getClipData() != null) {
-                var count = data1.clipData!!.itemCount
-                for (i in 0..count - 1) {
-                    var imageUri: Uri = data1!!.clipData!!.getItemAt(i).uri
-                    getPathFromURI(imageUri)
-                }
-            } else if (data1.getData() != null) {
-                var imagePath: String = data1.data!!.path!!
-                Log.e("imagePath", imagePath);
-            }
-
-//            displayImageData()
+        if (ImagePicker.shouldHandle(requestCode, resultCode, data1)) {
+            // Get a list of picked images
+           val images = ImagePicker.getImages(data1)
+            // or get a single image only
+            val image = ImagePicker.getFirstImageOrNull(data1)
         }
 
     }
